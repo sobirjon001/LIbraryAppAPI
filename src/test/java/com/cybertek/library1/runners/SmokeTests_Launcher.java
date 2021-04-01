ac72800fd8f4acda8705c4da379b1f116320fc1f
sobirjon001 <sobirjon001@gmail.com>
2021-03-31 20:25:46 -0400

    Sobir minor fix

--- src/test/java/com/cybertek/library1/runners/SmokeTests_Launcher.java
+++ src/test/java/com/cybertek/library1/runners/SmokeTests_Launcher.java
@@ -2,12 +2,10 @@ package com.cybertek.library1.runners;
 
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.core.LauncherConfig;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
-import org.junit.platform.reporting.legacy.xml.LegacyXmlReportGeneratingListener;
 
 import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
