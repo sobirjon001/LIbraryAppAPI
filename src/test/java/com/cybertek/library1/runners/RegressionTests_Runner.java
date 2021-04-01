fed2d8f4fc2ae09ac3b911e3aa824aad755f2c29
sobirjon001 <sobirjon001@gmail.com>
2021-03-31 21:06:15 -0400

    Sobir update in maven-surefire-plugin to set run specific runner

--- src/test/java/com/cybertek/library1/runners/RegressionTests_Runner.java
+++ src/test/java/com/cybertek/library1/runners/RegressionTests_Runner.java
@@ -2,6 +2,7 @@ package com.cybertek.library1.runners;
 
 
 import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.SuiteDisplayName;
 import org.junit.runner.RunWith;
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 // this is JUnit 4 runner. It needs to be correctly setup to recognize JUnit5 Tests
 @RunWith(JUnitPlatform.class)
 @SuiteDisplayName("Regression Tests")
-@SelectPackages("com/cybertek/library1/testCases")
-public class Includes_API_Regression_Test {
+@SelectPackages("com.cybertek.library1.testCases")
+@IncludeTags("regression")
+public class RegressionTests_Runner {
 }
